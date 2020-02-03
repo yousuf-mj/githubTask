e9e90b270823c898a9bd73ad0eb33f58663b8814
yousuf mj <yousuf.jaleel@dvcorporate.com>
2020-02-03 22:58:30 +0000

    complete function for getUser

--- src/github/index.ts
+++ src/github/index.ts
@@ -4,7 +4,6 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const getUser = (username: string) => {
-    
     const url = `${process.env.GITHUB_API}/users/${username}`
     return axios.get(url);
 }
--- tests/github/github.test.ts
+++ tests/github/github.test.ts
@@ -1,13 +1,23 @@
-import {getUser} from '../../src/github/index'
+import {getUser} from '../../src/github/index';
+import axios, {AxiosResponse} from 'axios'
+
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<typeof axios>;
 
 describe('Github API', () => {
+    let username: string;
+
+    beforeEach(() => {
+        username = 'yousuf-mj'
+    });
     
     it('should get basic information about a github user', async() => {
+        const mockResponse = require('../fixtures/github/userResponse.json');
+        mockedAxios.get.mockResolvedValue(mockResponse);
         
-        const user = await getUser('yousuf-mj');
-        console.log(user);
-        
-        
+        const user = await getUser(username);
+
         expect(user.status).toEqual(200);
+        expect(user.data.login).toEqual(username);
     });
 });
